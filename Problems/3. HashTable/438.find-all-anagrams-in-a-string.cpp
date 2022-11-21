9be56fe14b5d37e0e7962226f20a126ebcf8559c
M1kanN <fangliu2896@gmail.com>
2022-11-21 20:01:22 +0800

    hashTable Problems

---
+++ Problems/3. HashTable/438.find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=cpp
+ *
+ * [438] Find All Anagrams in a String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+
+    }
+};
+// @lc code=end
+
