94ca6ad332c0bd6d0a30c61868d8db54eb15a11c
M1kanN <fangliu2896@gmail.com>
2022-12-08 13:21:30 +0800

    Start Stack & Queue

---
+++ Problems/5. Stack&Queue/1047.remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=cpp
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+    }
+};
+// @lc code=end
+
